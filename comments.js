4f3d781e74ec1339d03bec884a06e78e0ce3523f
Shawn Sirigomez <151925669+shawnsirigomez@users.noreply.github.com>
2023-12-16 08:36:29 +0000

    Copilot thrid commit

--- comments.js
+++ comments.js
@@ -1,6 +1,47 @@
-//create web server
-var http = require('http');
+//Create web server
+var express = require('express');
+var app = express();
+
+//Create server
+var server = require('http').createServer(app);
+//Create socket
+var io = require('socket.io')(server);
+//Create file system
 var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var path = require('path');
+
+//Listen on port 3000
+server.listen(3000);
+
+//Array to store comments
+var comments = [];
+
+//Read comments from file
+fs.readFile('comments.txt', function(err, data) {
+  if(err) throw err;
+  comments = JSON.parse(data);
+});
+
+//When socket connection is established
+io.on('connection', function(socket) {
+  console.log('A user connected');
+  //When user disconnects
+  socket.on('disconnect', function() {
+    console.log('A user disconnected');
+  });
+  //When a user adds a comment
+  socket.on('addComment', function(comment) {
+    comments.push(comment);
+    //Write comments to file
+    fs.writeFile('comments.txt', JSON.stringify(comments), function(err) {
+      if(err) throw err;
+      console.log('Comment saved');
+    });
+    //Send updated comments to all users
+    io.emit('updateComments', comments);
+  });
+});
+
+//Send comments to client
+app.get('/comments', function(req, res) {
+  res.send(comments);
+});
